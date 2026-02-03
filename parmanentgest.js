d705d4b53e16c88d28ee3ce085c1d3e9b5290681
jahidsharuar2021-prog <jahidsharuar2021@gmail.com>
2026-02-03 19:05:28 +0600

    total item cost

--- parmanentgest.js
+++ parmanentgest.js
@@ -1,15 +1,16 @@
 
 const guest=[
-    {name:'shampo',price:100},
-    {name:'pant',price:1200},
-    {name:'shirt',price:500},
-    {name:'gamsha',price:200}
-
+    {name:'shampo',price:100 ,quantity:1},
+    {name:'pant',price:1200  ,quantity:2},
+    {name:'shirt',price:500  ,quantity:3},
+    {name:'gamsha',price:200 ,quantity:4}
 ];
+
 function guestdemand(items){
  let sum=0;
     for(const item of items){
-        sum=sum+item.price;
+       const productquantity=item.price*item.quantity;
+        sum=sum+productquantity;
     }
     return sum;
 }
