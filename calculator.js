19a9e57196a2e1878d4792904eba4b16fbef0f3d
jahidsharuar2021-prog <jahidsharuar2021@gmail.com>
2026-02-04 10:19:55 +0600

    calcultor design

---
+++ calculator.js
@@ -0,0 +1,42 @@
+
+
+function additon(a,b){
+    const add=a+b;
+    return add;
+}
+function substraction(a,b){
+    const sub=a-b;
+    return sub;
+}
+function multiplecation(a,b){
+    const multiply=a*b;
+    return multiply;
+}
+function division (a,b){
+    const div=a/b;
+    return div;
+}
+function calculator(a,b,operation){
+    if(operation==='add'){
+        const result=additon(a,b);
+        return result;
+    }
+    else if(operation==='sub'){
+        const result=substraction(a,b);
+        return result;
+    }
+    else if(operation==='multiply'){
+        const result=multiplecation(a,b);
+        return result;
+    }
+    else if(operation==='div'){
+        const result=division(a,b);
+        return result;
+    }
+    else{
+        
+        return "only 'sum','sub','multiply','div'";
+    }
+}
+const output=calculator(5,5,'multiply');
+console.log(output);
